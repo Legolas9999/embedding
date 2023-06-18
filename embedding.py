4cb3c3a9fb80d15a89d894ef760c232f7d268be3
yoga <yoga@DESKTOP-9RNBP77>
2023-06-18 16:54:16 +0900

    添加项目文件。

---
+++ embedding.py
@@ -0,0 +1,12 @@
+﻿import networkx as nx
+from minorminer import busclique
+import matplotlib.pyplot as plt
+import dwave_networkx as dnx
+
+triangle_graph = nx.complete_graph(3)
+triangle_edge = triangle_graph.edges()
+node_colors = [[0.8,.34,.167], 'yellow', 'orange']
+nx.draw(triangle_graph,with_labels = True,node_color = node_colors)
+plt.show()
+
+print(triangle)
